4f9f3cfe04f3ce4adc2191d529a266fe70b8695f
Kótai Dániel <dkotai66@gmail.com>
2022-09-18 14:03:37 +0200

    Feladat befejezve

--- src/Kor/Kor.java
+++ src/Kor/Kor.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Kor {
+package Kor;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Kor {
+    private int x;
+    private int y;
+    private int sugar;
+
+    Random rnd = new Random();
+
+    public Kor(int x, int y, int sugar){
+        this.x = x;
+        this.y = y;
+        this.sugar = sugar;
+    }
+
+    public Kor(int sugar){
+        this.x = 0;
+        this.y = 0;
+        this.sugar = sugar;
+    }
+
+    public Kor(){
+        this.x =  rnd.nextInt(50+10)-30;
+        this.y =  rnd.nextInt(50+10)-30;
+        this.sugar = rnd.nextInt(30);
+
+    }
+
+    public int getX(int x){
+        return x;
+    }
+
+    public int getY(int y){
+        return y;
+    }
+
+    public void setX(){
+        this.x = x;
+    }
+
+    public void setY(){
+        this.y = y;
+    }
+
+    public int getSugar(int sugar){
+        return sugar;
+    }
+
+    public void getSugar(){
+        this.sugar = sugar;
+    }
+
+    private int koordinataGeneralasa(int n) {
+        return (int) (Math.random() * (2 * n + 1)) - n;
+    }
+
+    public double Kerulet(){
+        return (this.sugar*2)*Math.PI;
+    }
+
+    public double Terulet(){
+        return (this.sugar*this.sugar)*Math.PI;
+    }
+
+    public double Meretezes(int szam){
+        return this.sugar+szam;
+    }
+
+
+    @Override
+    public String toString(){
+        return String.format("(%d,%d) koordinátájú, %d sugarú kör",this.x, this.y, this.sugar);
+    }
+
 }
--- src/Kor/Main.java
+++ src/Kor/Main.java
@@ -1,2 +1,49 @@
-package Kor;public class Main {
+package Kor;
+
+import Kor.Kor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Kor K1=new Kor(20, -10, 5);
+        Kor K2=new Kor(60);
+        Kor K3=new Kor();
+
+        System.out.println(K1);
+        System.out.println(K2);
+        System.out.println(K3);
+
+        System.out.printf("A kör kerülete %.2f", K3.Kerulet());
+        System.out.printf("A kör területe %.2f", K3.Terulet());
+
+        System.out.println("\nAdja meg a számot amivel méretezni szeretné a kört:  ");
+        int szam = sc.nextInt();
+        if (K3.Meretezes(szam)<0){
+            System.out.println("Hiba");
+        }
+        else{
+            System.out.printf("A sugár %s -re nőtt", K3.Meretezes(szam));
+        }
+
+
+        Kor korok[] = new Kor[10];
+        for (int i = 0; i< korok.length; i++){
+        korok[i]=new Kor();
+        }
+
+        for (Kor k:korok){
+            System.out.println(k);
+        }
+
+        int legnagyobbTeruletuKor =0;
+        for (int i = 0; i < korok.length; i++){
+            if(korok[legnagyobbTeruletuKor].Terulet()<korok[i].Terulet()){
+                legnagyobbTeruletuKor =i;
+            }
+        }
+
+        System.out.printf("A legnagyobb területtelrendelkező kör a %s. Területe %.3f cm",korok[legnagyobbTeruletuKor],korok[legnagyobbTeruletuKor].Terulet());
+    }
 }
